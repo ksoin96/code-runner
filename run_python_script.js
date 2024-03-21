40421d77de55daab30bd6cfb224937305a8d4dd2
ksoin96 <155584509+ksoin96@users.noreply.github.com>
2024-03-21 16:35:23 -0400

    Create run_python_script.js

---
+++ run_python_script.js
@@ -0,0 +1,19 @@
+function runCode() {
+    var code = editor.getValue();
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = "Output:<br>";
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8000/run_python_script", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            outputDiv.innerHTML += xhr.responseText;
+        }
+    };
+    xhr.send(JSON.stringify({ code: code }));
+}
+
+function clearOutput() {
+    document.getElementById("output").innerHTML = "";
+}
